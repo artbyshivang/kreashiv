731851c925a9237fc9c08327800a29d10861a70e
artbyshivang <heyshivang.design@gmail.com>
2026-05-19 22:27:15 +0530

    Fix Firebase Admin SDK path and add support for Env Var

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,11 +1,17 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    new URL("../my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json", import.meta.url)
-  )
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync(
+      new URL("./my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json", import.meta.url)
+    )
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
