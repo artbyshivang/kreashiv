e454508f70597064ef4a31b2414fefb84bc5ddfa
artbyshivang <heyshivang.design@gmail.com>
2026-05-19 19:29:17 +0530

    fix json import issue

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,11 @@
 import admin from "firebase-admin";
+import fs from "fs";
 
-import serviceAccount from "../my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json" assert { type: "json" };
+const serviceAccount = JSON.parse(
+  fs.readFileSync(
+    new URL("../my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json", import.meta.url)
+  )
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
