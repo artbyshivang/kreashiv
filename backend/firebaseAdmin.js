c42d5f4871a732352f7548a87b38b949fb964526
artbyshivang <heyshivang.design@gmail.com>
2026-05-19 19:25:36 +0530

    fix firebase json path

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,12 +1,9 @@
 import admin from "firebase-admin";
 
-import serviceAccount
-from "./my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json";
+import serviceAccount from "../my-kreashiv-app-firebase-adminsdk-fbsvc-08dd24a212.json" assert { type: "json" };
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    serviceAccount
-  ),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
